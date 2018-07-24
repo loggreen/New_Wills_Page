421f72b675a7ea784d148eb27a7b6fe16477da26
loggreen <logan@unglesbygreenberg.com>
2018-07-24 13:14:30 -0500

    logging

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var app = express();
 var mysql = require('mysql');
 
 var pool = mysql.createPool({
-	host 	: 'us-cdbr-iron-east-04.cleardb.net',
-	user	: 'b831e8211cb2c2',
-	password : 'b85560e',
-	database : 'heroku_690d259dc9aedcf?reconnect=true' 
+	host : process.env.DB_HOST,
+	user : process.env.DB_USERNAME,
+	password : process.env.DB_PASSWORD,
+	database : process.env.DB_DATABASE 
 
 });
 
