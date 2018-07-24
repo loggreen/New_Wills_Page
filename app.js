2bdd2934b1181f1e4ea48538f0bdc3d0ad494806
loggreen <logan@unglesbygreenberg.com>
2018-07-24 12:06:48 -0500

    fixed the host, user, pw info

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var app = express();
 var mysql = require('mysql');
 
 var pool = mysql.createPool({
-	host 	: '127.0.0.1',
-	user	: 'root',
-	password : 'root',
-	database : 'Practice_Blog_Table' 
+	host 	: 'us-cdbr-iron-east-04.cleardb.net',
+	user	: 'b831e8211cb2c2',
+	password : 'b85560e',
+	database : 'heroku_690d259dc9aedcf?reconnect=true' 
 
 });
 
