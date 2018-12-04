612e6182e562957d861a45d5e3050354f253dfb4
Geoffrey Gauchet <geoff@zhephree.com>
2018-12-04 15:49:12 -0600

    email form sendgrid api; handle internal links

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
+var fs = require('fs');
+var helper = require('sendgrid').mail;
+var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
 
 var pool = mysql.createPool({
 	host : process.env.DB_HOST,
@@ -30,9 +33,49 @@ app.get('/', function(req, res) {
 
 	console.log(path.resolve('Public/index.html'))
 
-   res.sendFile(path.resolve('Public/index.html'));
+   res.send(getHTML('index.html'));
  
  })
+
+/* 
+you can make up pretty much any URL you want for the subpages.
+in the nav.html file, just point the link to the URL you want
+for the page. eg: /successions means you'll have 
+sunriselawgroup.com/successions as the URL for that page.
+once you have the link in the nav set, come here and add a new 
+route to the URL you just came up with. The getHTML() function
+is a custom function I made that inserts your nav.html file
+into the nav section of each page so you only ever have to update
+the nav.html and not every page on your site. The getHTML function
+gets the file you specify that's in the Public folder, with the
+proper nav links inserted into it.
+
+i put your practice area pages under a fake /practice-areas folder
+to keep the URLs consistent, and it'll help with SEO.
+
+I've already created the routes and updated your nav.html. You'll just
+have to create the missing HTML files
+*/
+
+app.get('/videos', function(req, res) {
+	res.send(getHTML('Videos.html'))
+});
+app.get('/events', function(req, res) {
+	res.send(getHTML('Events.html'))
+});
+
+
+app.get('/practice-areas/wills', function(req, res) {
+	res.send(getHTML('Wills.html'))
+});
+
+app.get('/practice-areas/successions', function(req, res) {
+	res.send(getHTML('Successions.html'))
+});
+
+app.get('/practice-areas/power-of-attorney', function(req, res) {
+	res.send(getHTML('Power_of_Attorney.html'))
+});
 
 
 app.get('/Practice_Blog_Table/Blogs', function(req,res){
@@ -91,6 +134,55 @@ app.get('/Full_Blog_Pull/Blogs', function(req,res){
 
 
 app.post('/Sunrise_Law_Group/leads', function(req,res){ 
+	//generate the email body
+	var mail_body = '';
+	for(var key in req.body){
+		mail_body += '<b>' + key + ':</b> ' + req.body[key] + "<br>";
+	}
+
+	//build the SendGrid API request
+	var request = sg.emptyRequest({
+	  method: 'POST',
+	  path: '/v3/mail/send',
+	  body: {
+	    personalizations: [
+	      {
+	        to: [
+	          {
+	            email: 'YOUR_EMAIL_GOES_HERE',
+	          },
+	          {
+	            email: 'YOUR_OTHER_EMAIL_GOES_HERE',
+	          },
+	        ],
+	        subject: 'New Lead from Website',
+	      },
+	    ],
+	    from: {
+	      email: 'leads@sunriselawgroup.com',
+	      name: 'Sunrise Law Group Website'
+	    },
+	    content: [
+	      {
+	        type: 'text/html',
+	        value: mail_body,
+	      },
+	    ],
+	  },
+	});
+
+	//Send the email via SendGrid
+	sg.API(request)
+	  .then(response => {
+	    console.log(response.statusCode);
+	    console.log(response.body);
+	    console.log(response.headers);
+	  })
+	  .catch(error => {
+	    //error is an instance of SendGridError
+	    //The full response is attached to error.response
+	    console.log(error.response.statusCode);
+	  });
 
 	console.log("made it to the app.post!")
 	console.log("BODY: ", req.body);
@@ -103,26 +195,33 @@ app.post('/Sunrise_Law_Group/leads', function(req,res){
 	console.log("NAME: ", Name);
 
 
-	pool.getConnection(function(err, connection){
-		var sqlquery = "INSERT INTO leads (Name, Phone_Number, Email, Message) VALUES ('"+Name+"','"+Phone+"','"+Email+"','"+Message+"')"
-		console.log(sqlquery);
-		connection.query(sqlquery, function (error, results, fields){
-			if (error) {
-				console.log("ERROR", error);
-			}
-			connection.release();
-			if(!err) {
-				res.send({"success":"true"});
-			}
-		});
-	});
+	// pool.getConnection(function(err, connection){
+	// 	var sqlquery = "INSERT INTO leads (Name, Phone_Number, Email, Message) VALUES ('"+Name+"','"+Phone+"','"+Email+"','"+Message+"')"
+	// 	console.log(sqlquery);
+	// 	connection.query(sqlquery, function (error, results, fields){
+	// 		if (error) {
+	// 			console.log("ERROR", error);
+	// 		}
+	// 		connection.release();
+	// 		if(!err) {
+	// 			res.send({"success":"true"});
+	// 		}
+	// 	});
+	// });
 });
 
+
+function getHTML(file){
+	var html = fs.readFileSync(path.resolve('Public/' + file), 'utf8');
+	var nav = fs.readFileSync(path.resolve('nav.html'), 'utf8');
+
+	return html.replace('{% nav %}', nav);
+}
 
 
 var port = process.env.PORT || 3000
 
-app.listen(port, function() {console.log('Heyoooo!')})
+app.listen(port, function() {console.log('Heyoooo!');})
 
 
 
