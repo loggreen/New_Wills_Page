f6b2b580f78effe37fb523cfea9c5d7fa00125c3
loggreen <logan@unglesbygreenberg.com>
2019-01-11 10:28:14 -0600

    post_date

--- Public/Practice_Blog.js
+++ Public/Practice_Blog.js
@@ -41,17 +41,17 @@ function sidebar_blog_pull(){
 
                 "<div class='vlog'><iframe class='video sidebar_video' height='202' src='" + data[0].Video_Code + "' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe></div>"
                 + "<h6 class='post_title' id='first_post_title'>" + data[0].Headline + "</h6>"
-                + "<h6 class='video_run_time' id='first_video_run_time'>" + data[0].Run_Time + "</h6>"
+                + "<h6 class='video_run_time' id='first_video_run_time'>" + data[0].Post_Date + "</h6>"
                 + "<p class='post_preview' id='first_post_preview'>" + data[0].Text_Preview + "</p>"
 
                 + "<div class='vlog'><iframe class='video sidebar_video' height='202' src='" + data[1].Video_Code + "' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe></div>"
                 + "<h6 class='post_title' id='first_post_title'>" + data[1].Headline + "</h6>"
-                + "<h6 class='video_run_time' id='first_video_run_time'>" + data[1].Run_Time + "</h6>"
+                + "<h6 class='video_run_time' id='first_video_run_time'>" + data[1].Post_Date + "</h6>"
                 + "<p class='post_preview' id='first_post_preview'>" + data[1].Text_Preview + "</p>"
 
                 + "<div class='vlog'><iframe class='video sidebar_video' height='202' src='" + data[2].Video_Code + "' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe></div>"
                 + "<h6 class='post_title' id='first_post_title'>" + data[2].Headline + "</h6>"
-                + "<h6 class='video_run_time' id='first_video_run_time'>" + data[2].Run_Time + "</h6>"
+                + "<h6 class='video_run_time' id='first_video_run_time'>" + data[2].Post_Date + "</h6>"
                 + "<p class='post_preview' id='first_post_preview'>" + data[2].Text_Preview + "</p>"
               
             );
