cfebfbd9259d494c24ec5ec50e8e5ef6416cb21a
Geoffrey Gauchet <ggauchet@lookfar.com>
2019-01-22 11:38:36 -0600

    test loading all blogs

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ app.get('/blogs/:start/:count', function(req,res){
 
 		console.log('error', err)
 		if(connection){
-			connection.query('SELECT * FROM final_blogs ORDER BY Blog_ID DESC LIMIT ' + start + ' OFFSET ' + count, function(error, result, fields){
+			connection.query('SELECT * FROM final_blogs ORDER BY Blog_ID DESC', function(error, result, fields){
 				console.log('/blog error', error, result);
 				connection.release();
 
