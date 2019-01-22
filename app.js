9c99afc030fa6a5726331b015b76b3b8a59af193
Geoffrey Gauchet <ggauchet@lookfar.com>
2019-01-22 15:19:07 -0600

    fix https redirect

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ console.log(process.env.DB_DATABASE)
 function requireHTTPS(req, res, next) {
   // The 'x-forwarded-proto' check is for Heroku
   if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
-    return res.redirect('https://www.' + req.get('host') + req.url);
+    return res.redirect('https://' + req.get('host') + req.url);
   }
   next();
 }
